e2bd17fa53fbd300f28c901c4331c8f26b992edb
Manju Rajashekhar <manj@twitter.com>
2013-01-16 14:33:43 -0800

    updated run-all.sh

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -14,12 +14,14 @@ call_rate=0
 lb_size=100
 lb_size=1000
 
-n=100
+prefix="abc:"
 
-for i in `seq 1 ${n}`; do
+niter=100
+
+for i in `seq 1 ${niter}`; do
     for method in "set" "get" "gets" "delete" "add" "incr" "decr" "replace" "append" "prepend"; do
         printf "[%d] command: %s =>\n" $i $method
-        ${mcperf} --server=${server} --port=${port} --num-conns=${num_conns} --conn-rate=${conn_rate} --num-calls=${num_calls} --call-rate=${call_rate} --method=${method} --sizes=u${lb_size},${lb_size} >> /tmp/mcperf.out 2>&1
+        ${mcperf} --server=${server} --port=${port} --num-conns=${num_conns} --conn-rate=${conn_rate} --num-calls=${num_calls} --call-rate=${call_rate} --method=${method} --sizes=u${lb_size},${lb_size} --prefix=${prefix} >> /tmp/mcperf.out 2>&1
     done
     grep --color "error\|Errors:" /tmp/mcperf.out | grep -v "0"
     mv /tmp/mcperf.out /tmp/mcperf.out.${i}
